9188798b5af53bd9b08cd03b36eb59b965be13a0
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-03-22 10:26:31 -0500

    Initial data

---
+++ Assets/_Game/Scripts/MouseLook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform player;
+
+    [SerializeField] private float m_verticalSensitivity = 1;
+    [SerializeField] private float m_horizontalSensitivity = 1;
+
+    private float m_xRotation = 0;
+
+    void Start()
+    {
+        // Lock cursor to game view
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Get x and y mouse input
+        float mouseX = Input.GetAxis("Mouse X") * m_horizontalSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * m_verticalSensitivity * Time.deltaTime;
+
+        // Get new x rotation
+        m_xRotation -= mouseY;
+
+        // Clamp x rotation
+        m_xRotation = Mathf.Clamp(m_xRotation, -90, 90);
+
+        // Rotate camera on x
+        transform.localRotation = Quaternion.Euler(m_xRotation, 0, 0);
+
+        // Rotate player on z
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/_Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float m_targetSpeed = 10;
+    [SerializeField] private float m_jumpHeight = 2;
+    [SerializeField] private float m_gravity = -9.81f;
+    [Space]
+    [SerializeField] private Vector3 m_groundCheck = new Vector3();
+    [SerializeField] private float m_groundCheckRadius = 1;
+    [SerializeField] private LayerMask m_whatIsGround = new LayerMask();
+
+    private CharacterController m_controller;
+
+    private float m_speed;
+    private Vector3 m_velocity;
+
+    private bool m_isGrounded;
+
+    void Start()
+    {
+        m_controller = GetComponent<CharacterController>();
+        m_speed = m_targetSpeed;
+    }
+
+    void Update()
+    {
+        m_isGrounded = Physics.CheckSphere(m_groundCheck, m_groundCheckRadius, m_whatIsGround);
+
+        // Reset y velocity when grounded
+        if (m_isGrounded && m_velocity.y < 0)
+            m_velocity.y = -2f;
+
+        // Get x and y keyboard input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Move player on local axes
+        Vector3 movement = transform.right * x + transform.forward * z;
+        m_controller.Move(movement * m_speed * Time.deltaTime);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && m_isGrounded)
+            m_velocity.y = Mathf.Sqrt(m_jumpHeight * -2f * m_gravity);
+
+        // Gravity
+        m_velocity.y += m_gravity * Time.deltaTime;
+        m_controller.Move(m_velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position + m_groundCheck, m_groundCheckRadius);
+    }
+}
